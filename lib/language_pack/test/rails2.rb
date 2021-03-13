b2a5478d97bd91182c7a7a9aaa3c0837b1bfac38
Josh Kinabrew <josh@recklesspursuit.com>
2021-03-12 22:09:45 -0700

    create all databases when running tests

--- lib/language_pack/test/rails2.rb
+++ lib/language_pack/test/rails2.rb
@@ -29,7 +29,7 @@ class LanguagePack::Rails2
     # db:test:purge is called by everything in the db:test namespace
     # db:create is called by :db:schema:load_if_ruby
     # db:structure:dump is not needed for tests, but breaks Rails 3.2 db:structure:load on Heroku
-    ["db:test:purge", "db:create", "db:structure:dump"]
+    ["db:test:purge", "db:create:all", "db:structure:dump"]
   end
 
   # rails test runner + rspec depend on db:test:purge which drops/creates a db which doesn't work on Heroku's DB plans
